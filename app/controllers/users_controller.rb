ee10fba92722d8e55bfc7f0f57bf83559d1c04d9
Xander Hendry <vhendry.coding@gmail.com>
2023-12-12 17:31:32 -0500

    add session cookie to user login action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -22,6 +22,7 @@ class UsersController <ApplicationController
   def login
     user = User.find_by(email: params[:email])
     if user.authenticate(params[:password])
+      session[:user_id] = user.id
       redirect_to user_path(user.id)
     else
       flash[:error] = "Sorry, your credentials are bad."
