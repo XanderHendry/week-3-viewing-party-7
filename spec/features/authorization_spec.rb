7b5b0b98381d4a56a29c7f13f66d56880d07d7e4
Xander Hendry <vhendry.coding@gmail.com>
2023-12-12 18:09:54 -0500

    updated exitsting users section to display for registered user, but not as links to their show pages

--- spec/features/authorization_spec.rb
+++ spec/features/authorization_spec.rb
@@ -8,4 +8,24 @@ RSpec.describe 'App Authorizations' do
       expect(page).to_not have_content('Existing Users')
     end
   end
+
+  describe 'Landing page as Registered User' do 
+    it 'displays the list of existing users, but their emails are not links.' do 
+    user1 = User.create(name: "User One", email: "user1@test.com", password: 'test', password_confirmation: 'test')
+    user2 = User.create(name: "User Two", email: "user2@test.com", password: 'test', password_confirmation: 'test')
+
+    visit login_path
+
+    fill_in :email, with: user1.email
+    fill_in :password, with: user1.password
+
+    click_button 'Log In'
+    
+    visit root_path
+
+    expect(page).to have_content('Existing Users')
+    expect(page).to have_content(user2.email)
+    expect(page).to_not have_link(user2.email)
+    end
+  end
 end 
