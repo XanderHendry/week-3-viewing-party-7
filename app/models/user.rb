751402ff18db80bcc59bc24c833a1a410ed2bf21
Xander Hendry <vhendry.coding@gmail.com>
2023-12-12 16:14:08 -0500

    starting sessions challenge

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,8 @@
 class User <ApplicationRecord 
   validates_presence_of :email, :name, :password
   validates_confirmation_of :password
-  validates_uniqueness_of :email
+  validates :email, uniqueness: { case_sesitive: false}
   has_secure_password
-  has_many :viewing_parties
 
+  has_many :viewing_parties
 end 
